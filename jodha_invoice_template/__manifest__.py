8315ec78f4a7bed6dc7425ca00df006af7f11a39
hafzalabbas <36755974+hafzalabbas@users.noreply.github.com>
2020-03-26 21:51:33 +0530

    manifst_edit

--- jodha_invoice_template/__manifest__.py
+++ jodha_invoice_template/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "todo_app",
+    'name': "Jodha Invoice Template",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
